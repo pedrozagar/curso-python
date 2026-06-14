27ceb20aa91642fa5e70a136c613862b993c49ae
pedrozagar <pedro.zagar2@gmail.com>
2026-06-14 14:49:15 -0300

    primeiros passos no python

---
+++ exemplo01.py
@@ -0,0 +1,14 @@
+#exercícios em python:
+
+print("oi")
+
+numero1 = int(input("digite um valor: "))
+numero2 = int(input("digite um valor: "))
+
+resposta = numero1 + numero2
+print("resposta ", resposta)
+
+numero1, numero2 = numero2, numero1
+
+print("numero 1: ", numero1)
+print("numero 2: ", numero2)
